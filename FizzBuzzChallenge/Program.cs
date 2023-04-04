d739bbbf6090294aca95581236e4b150234e61e4
Alec <alec.m.hancock@gmail.com>
2023-04-04 11:34:15 -0500

    original answer in comment, used tuple pattern to clean

--- FizzBuzzChallenge/Program.cs
+++ FizzBuzzChallenge/Program.cs
@@ -11,15 +11,26 @@
         }
         static string FizzBuzz(double num)
         {
-            string output = "";
-            if (num % 3 == 0 && num % 5 == 0)
-            { return output += "fizzbuzz"; }
-            else if (num % 5 == 0)
-            { return output += "buzz"; }
-            else if (num % 3 == 0)
-            { return output += "fizz"; }
-            else
-            { return output += "Input number not divisible by 3 or 5!"; }
+            //string output = "";
+            //if (num % 3 == 0 && num % 5 == 0)
+            //{ return output += "fizzbuzz"; }
+            //else if (num % 5 == 0)
+            //{ return output += "buzz"; }
+            //else if (num % 3 == 0)
+            //{ return output += "fizz"; }
+            //else
+            //{ return output += "Input number not divisible by 3 or 5!"; }
+
+            
+            {
+                return (num % 3, num % 5) switch
+                {
+                    (0, 0) => "FizzBuzz",
+                    (0, _) => "Fizz",
+                    (_, 0) => "Buzz",
+                    (_, _) => "Input number not divisible by 3 or 5!"
+                };
+            }
         }
     }
 }
